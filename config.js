c9ae98d235f4dd16521f9ff9381b372504b08605
kavidumdbot <herokobot4@gmail.com>
2025-08-14 11:05:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YGtHlATT#CwZkTOsXAaPyBLrkBW7VTgSUJhJ7RSnPktev-H31cKg",
+SESSION_ID: process.env.SESSION_ID || "sURwVQ4T#6nXM7A5v1YBAoslXiesVpOogiTz8uuOUoW94egxWMnc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PODDAe/DULA-MD-V1.0/blob/main/IMAGES/DULA-MD-LOGO.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DULA-MD Is Alive Now😍*",
 BOT_OWNER: '94752978237',  // Replace with the owner's phone number
